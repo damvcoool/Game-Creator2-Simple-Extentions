9da5052e71af4eb7511ace92dbff17a8c586c3a1
Daniel Mejia <dmejia@tresmomkeysoup.net>
2024-05-13 09:56:30 -0400

    Simple Settings
    Work in Progress.

---
+++ Assets/Plugins/SimpleExtentions/SimpleSettings/Runtime/VisualScripting/ConditionSimpleSettingsHasSavedSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using GameCreator.Runtime.Common;
+using GameCreator.Runtime.VisualScripting;
+
+[Serializable]
+public class ConditionSimpleSettingsHasSavedSettings : Condition
+{
+    protected override bool Run(Args args)
+    {
+        return true;
+    }
+}
---
+++ Assets/Plugins/SimpleExtentions/SimpleSettings/Runtime/VisualScripting/InstructionSimpleSettingsLoadSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using GameCreator.Runtime.Common;
+using GameCreator.Runtime.VisualScripting;
+
+[Serializable]
+public class InstructionSimpleSettingsLoadSettings : Instruction
+{
+    protected override Task Run(Args args)
+    {
+        // Your code here...
+        return DefaultResult;
+    }
+}
